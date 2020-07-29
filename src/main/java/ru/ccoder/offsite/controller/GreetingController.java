c0105869161cb1a0d1742cc371b8ac05ade147d4
Alexey <antizep@mail.ru>
2020-07-29 20:28:22 +0300

    Создан скелет оф сайта, добаленна веб версия игры.

---
+++ src/main/java/ru/ccoder/offsite/controller/GreetingController.java
@@ -0,0 +1,35 @@
+package ru.ccoder.offsite.controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+
+import lombok.extern.slf4j.Slf4j;
+
+@Controller
+@Slf4j
+public class GreetingController {
+
+	@GetMapping("/index")
+	public String greeting(Model model) {
+
+		/*
+		 * StockMarketService service = new StockMarketService(repository);
+		 * 
+		 * List<EntityTools> tools = service.getAllTools();
+		 * 
+		 * List<EntityOrder> orders = new ArrayList<EntityOrder>(); for(EntityOrder
+		 * e:orderRepository.findAll()) { orders.add(e); } log.info("Service" + tools);
+		 * model.addAttribute("lists", tools); model.addAttribute("orders",orders);
+		 */
+		
+		return "index";
+
+	}
+}
